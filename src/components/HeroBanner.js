88a0b97d056b82f183ec3d0b02bef9d9f4f5913f
NoobCoder_69 <vishant448@gmail.com>
2024-10-11 14:22:31 +0530

    Refactor HeroBanner component

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -6,71 +6,71 @@ import '../Styles/HeroBanner.css'
 
 export const HeroBanner = () => {
 
-    const [loopNum, setLoopNum] = useState(0)
-    const [isDeleting, setIsDeleting] = useState(false)
-    const toRotate = [ "Web Developer", "ML Enthusiast", "Problem Solver" ]
-    const [delta, setDelta] = useState(200);
-    const [index, setIndex] = useState(1);
-    const [text, setText] = useState('')
-    const period = 2000
-    const[activeLink, setActiveLink] = useState('home')
-    const onUpdateActiveLink = (value) => {
-      setActiveLink(value)
+  const [loopNum, setLoopNum] = useState(0)
+  const [isDeleting, setIsDeleting] = useState(false)
+  const toRotate = ["Web Developer", "ML Enthusiast", "Problem Solver"]
+  const [delta, setDelta] = useState(200);
+  const [index, setIndex] = useState(1);
+  const [text, setText] = useState('')
+  const period = 2000
+  const [activeLink, setActiveLink] = useState('home')
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value)
   }
 
-    useEffect(() => {
-        let ticker = setInterval(() => {
-          tick();
-        }, delta);
-    
-        return () => { clearInterval(ticker) };
-      }, [text])
-    
-      const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-    
-        setText(updatedText);
-    
-        if (isDeleting) {
-          setDelta(50);
-        }
-    
-        if (!isDeleting && updatedText === fullText) {
-          setIsDeleting(true);
-          setIndex(prevIndex => prevIndex - 1);
-          setDelta(period);
-        } else if (isDeleting && updatedText === '') {
-          setIsDeleting(false);
-          setLoopNum(loopNum + 1);
-          setIndex(1);
-          setDelta(200);
-        } else {
-          setIndex(prevIndex => prevIndex + 1);
-        }
-      }
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(50);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(200);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
 
 
-    return (
-        <section className="banner" id="home">
-          <Container>
-            <Row className="aligh-items-center">
-              <Col xs={12} md={6} xl={7}>
-                    <h1>{`Hi! I'm Rohit and I'm a `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "ML Enthusiast", "Problem Solver" ]'><span className="wrap">{text}</span></span></h1>
-                    <span className="tagline">Welcome to my Portfolio</span>
-                      <p>Your friendly neighborhood web developer here. I craft websites that work harder than I do and explore tech like it’s a buffet—sampling everything from front-end delicacies to backend surprises. Join me as I turn coffee into code and ideas into pixels!</p>
-                      <Nav.Link href="#footer" className={activeLink === 'footer' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('footer')}><button className='vvd'><span>Get in Touch<ArrowRightCircle size={25} /></span></button>
-             </Nav.Link>
-                
-              </Col>
-              {/* <Col>
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={6}>
+            <h1>{`Hi! I'm Rohit and I'm a `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "ML Enthusiast", "Problem Solver" ]'><span className="wrap">{text}</span></span></h1>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <p>Your friendly neighborhood web developer here. I craft websites that work harder than I do and explore tech like it's a buffet—sampling everything from front-end delicacies to backend surprises. Join me as I turn coffee into code and ideas into pixels!</p>
+            <Nav.Link href="#footer" className={activeLink === 'footer' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('footer')}><button className='vvd'><span>Get in Touch<ArrowRightCircle size={25} /></span></button>
+            </Nav.Link>
+
+          </Col>
+          {/* <Col>
               <div>
                   <img src={headerImg} alt="Header Img"/>
                 </div>
               </Col> */}
-            </Row>
-          </Container>
-        </section>
-      )
+        </Row>
+      </Container>
+    </section>
+  )
 }
